85b20d793c72e52814a4c41f02a9dbb4924767e7
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2021-05-17 10:59:04 -0700

    Initial commit.

---
+++ src/components/LocationHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class LocationHeader extends React.Component {
+    public render() {
+        return (
+            <div>Location Header</div>
+        );
+    }
+}
