94c89d71cb1ca0319c91f0501845cfa93c96bb7d
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2021-06-04 18:20:41 -0700

    Adds cargo to ships.

---
+++ src/Types/Constants.ts
@@ -0,0 +1,9 @@
+/** 
+ * The price of a pirate at port per turn on the easiest difficulty. Difficulty x this number.
+ */
+export const BasePirateWage = 2;
+
+/** 
+ * For the least expensive ship with a cost modifier of 1. A 10 would 10 x this number.
+ */
+export const BaseShipPrice = 10000;
