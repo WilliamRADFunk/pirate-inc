166792fe6a5ad8cc7a0f9f958744f8bb0f4be03d
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2021-06-07 10:59:21 -0700

    Adds some stat and state management.

--- src/services/PlayerManager.ts
+++ src/services/PlayerManager.ts
@@ -1,5 +1,21 @@
 import { BehaviorSubject, Observable } from "rxjs";
 
+export interface PlayerPositiveStats {
+    Iron_Will: number;
+    Cunning: number;
+    Energetic: number;
+    Silver_Tongue: number;
+    People_Person: number;
+}
+
+export interface PlayerNegativeStats {
+    Reckless: number;
+    Cowardly: number;
+    Lethargic: number;
+    Close_Minded: number;
+    Paranoid: number;
+}
+
 // Singleton to control the various aspects of the player object.
 class PlayerManager {
     /**
@@ -16,6 +32,22 @@ class PlayerManager {
      * The name the player has chosen for themself.
      */
     private playerName: string = '';
+
+    /**
+     * The name the player has chosen for themself.
+     */
+    private playerStats: BehaviorSubject<PlayerPositiveStats & PlayerNegativeStats> = new BehaviorSubject({
+        Iron_Will: 5,
+        Cunning: 5,
+        Energetic: 5,
+        Silver_Tongue: 5,
+        People_Person: 5,
+        Cowardly: 5,
+        Reckless: 5,
+        Lethargic: 5,
+        Close_Minded: 5,
+        Paranoid: 5
+    });
 
     /**
      * The number of action points player has left for their turn.
@@ -44,6 +76,14 @@ class PlayerManager {
     }
 
     /**
+     * Allows subscribing to the player's stats.
+     * @returns observable of the player's stats.
+     */
+    public getPlayerStats(): Observable<PlayerPositiveStats & PlayerNegativeStats> {
+        return this.playerStats.asObservable();
+    }
+
+    /**
      * Allows subscribing to the player's total possible action points.
      * @returns observable of the player's total possible action points for current turn.
      */
@@ -51,7 +91,7 @@ class PlayerManager {
         return this.totalActionPoints.asObservable();
     }
 
-    public initiatePlayer(difficulty: number, name: string): void {
+    public initiatePlayer(difficulty: number, name: string, stats: PlayerPositiveStats & PlayerNegativeStats): void {
         if (this.initiated) {
             return;
         }
--- src/services/StateManager.ts
+++ src/services/StateManager.ts
@@ -1,7 +1,7 @@
 import { BehaviorSubject, Observable } from "rxjs";
 
 /**
- * The main game states to control game flow from starting to playing to ending.
+ * The main game status to control game flow from starting to playing to ending.
  */
  export enum GameState {
     Start = 0,
@@ -11,12 +11,41 @@ import { BehaviorSubject, Observable } from "rxjs";
     GameOver = 4
 }
 
+/**
+ * The port scene status to control game flow while in a port.
+ */
+ export enum PortSceneState {
+    Menu = 0,
+    Event = 1,
+    Trial = 2,
+    BungalowShipManifest = 3,
+    BungalowCrewManifest = 4,
+    BungalowGatherIntel = 5,
+    BungalowOfficerSummaries = 6,
+    ShipyardBuy = 7,
+    ShipyardSell = 8,
+    ShipyardRepair = 9,
+    ShipyardOutfit = 10,
+    TavernFireCrew = 11,
+    TavernHireCrew = 12,
+    TavernFireOfficers = 13,
+    TavernHireOfficers = 14,
+    TavernBuySupplies = 15,
+    ColonialOfficeBribe = 16,
+    ColonialOfficeRoyalPardon = 17,
+    ColonialOfficeWritOfProtection = 18
+}
+
 // Singleton to control the various states of the game.
 class StateManager {
     /**
      * The state of the overall game: Menu, Intro, Active, Game Over, etc..
      */
     private gameState: BehaviorSubject<GameState> = new BehaviorSubject(GameState.Start as GameState);
+    /**
+     * The state of the port scene.
+     */
+    private portSceneState: BehaviorSubject<PortSceneState> = new BehaviorSubject(PortSceneState.Menu as PortSceneState);
 
     /**
      * Changes to different game state if new state is an allowed transition from current state.
@@ -42,6 +71,14 @@ class StateManager {
 
         this.gameState.next(newState);
     }
+    
+    /**
+     * Changes to different port scene state if new state is an allowed transition from current state.
+     * @param newState the new port scene state to transition to.
+     */
+    public changePortSceneState(newState: GameState): void {
+
+    }
 
     /**
      * Allows subscribing to the game's main state.
@@ -49,6 +86,14 @@ class StateManager {
      */
     public getGameState(): Observable<GameState> {
         return this.gameState.asObservable();
+    }
+
+    /**
+     * Allows subscribing to the port scene's state.
+     * @returns observable of the port scene's current main state.
+     */
+    public getPortSceneState(): Observable<PortSceneState> {
+        return this.portSceneState.asObservable();
     }
 }
 
