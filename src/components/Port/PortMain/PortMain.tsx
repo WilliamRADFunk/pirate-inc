dfd790ffea0d0aa8245c9435add6fc7477d106c3
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2021-06-30 15:30:21 -0700

    Small style adjustment.

--- src/components/Port/PortMain/PortMain.tsx
+++ src/components/Port/PortMain/PortMain.tsx
@@ -48,37 +48,37 @@ export class PortMain extends React.Component<Props, State> {
                 <Row>
                     { portSceneState > PortSceneState.TavernOptions ? null :
                         <>
-                            <Col xs="12" xl="5" className="mt-5 rope-border-square small-square">
+                            <Col xs="12" xl="5" className="rope-border-square small-square">
                                 <Shipyard></Shipyard>
                             </Col>
-                            <Col xs="12" xl={{ span: 5, offset: 2 }} className="mt-5 rope-border-square small-square">
+                            <Col xs="12" xl={{ span: 5, offset: 2 }} className="rope-border-square small-square">
                                 <Tavern></Tavern>
                             </Col>
-                            <Col xs="12" xl="5" className="mt-5 rope-border-square small-square">
+                            <Col xs="12" xl="5" className="rope-border-square small-square">
                                 <ColonialOffice></ColonialOffice>
                             </Col>
-                            <Col xs="12" xl={{ span: 5, offset: 2 }} className="mt-5 rope-border-square small-square">
+                            <Col xs="12" xl={{ span: 5, offset: 2 }} className="rope-border-square small-square">
                                 <Bungalow></Bungalow>
                             </Col>
                         </>
                     }
                     { (portSceneState < PortSceneState.BungalowCrewManifest || PortSceneState.BungalowShipManifest < portSceneState) ? null :
-                        <Col xs="12" className="boundaries mt-5">
+                        <Col xs="12" className="boundaries">
                             <Bungalow></Bungalow>
                         </Col>
                     }
                     { (portSceneState < PortSceneState.ColonialOfficeBribe || PortSceneState.ColonialOfficeWritOfProtection < portSceneState) ? null :
-                        <Col xs="12" className="boundaries mt-5">
+                        <Col xs="12" className="boundaries">
                             <ColonialOffice></ColonialOffice>
                         </Col>
                     }
                     { (portSceneState < PortSceneState.ShipyardBuy || PortSceneState.ShipyardSell < portSceneState) ? null :
-                        <Col xs="12" className="boundaries mt-5">
+                        <Col xs="12" className="boundaries">
                             <Shipyard></Shipyard>
                         </Col>
                     }
                     { portSceneState < PortSceneState.TavernBuySupplies ? null :
-                        <Col xs="12" className="boundaries mt-5">
+                        <Col xs="12" className="boundaries">
                             <Tavern></Tavern>
                         </Col>
                     }
