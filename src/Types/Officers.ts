703ba68e9a77d84051dcba3d8fb7297b7f4b7024
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2021-07-07 20:35:34 -0700

    Adds the shell for officer interface definitions.

---
+++ src/Types/Officers.ts
@@ -0,0 +1,15 @@
+export interface Officer {
+
+}
+
+export interface Carpenter extends Officer {
+
+}
+
+export interface Doctor extends Officer {
+
+}
+
+export interface Quartermaster extends Officer {
+
+}
